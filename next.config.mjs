8f3c3ba217dd2d534d5eeecb3ef4a8fe5875be0f
Justin <justin.eiletz@outlook.com>
2025-04-11 23:22:30 +0200

    Updates regarding JExcellence

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,80 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  sassOptions: {
-    compiler: "modern",
-    silenceDeprecations: ["legacy-js-api"],
-  },
+import mdx from "@next/mdx";
 
-  async rewrites() {
+const withMDX = mdx({
+  extension: /\.mdx?$/,
+  options: {}
+});
+
+const nextConfig = {
+  eslint: {
+    dirs: [],
+  },
+  poweredByHeader: false,
+  async redirects() {
     return [
       {
-        source: '/proxy/:path*',
-        destination: 'https://lmbeauty.de/:path*'
+        source: '/:path*',
+        has: [{
+          type: 'host',
+          value: 'www.jexcellence.de'
+        }],
+        destination: 'https://jexcellence.de/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'jexcellence.de'
+          },
+          {
+            type: 'header',
+            key: 'x-forwarded-proto',
+            value: 'http'
+          }
+        ],
+        destination: 'https://jexcellence.de/:path*',
+        permanent: true,
       }
-    ];
+    ]
+  },
+  headers: () => [
+    {
+      source: '/(.*)',
+      headers: [
+        { key: 'X-Content-Type-Options', value: 'nosniff' },
+        { key: 'X-Frame-Options', value: 'DENY' },
+        { key: 'X-XSS-Protection', value: '1; mode=block' },
+        {
+          key: 'Strict-Transport-Security',
+          value: 'max-age=63072000; includeSubDomains; preload'
+        },
+        {
+          key: 'Cache-Control',
+          value: 'public, s-maxage=3600, stale-while-revalidate=86400'
+        }
+      ],
+    },
+  ],
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
+  experimental: {
+    optimizeCss: true,
+    optimizeServerReact: true,
+  },
+  webpack(config) {
+    config.optimization.splitChunks = {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.(css|scss)$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    }
+    return config
   }
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
